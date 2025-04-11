103a896f1ca26e5edb5f1b2125880af3c99fef44
madhuri2904 <banoriyamadhuri@gmail.com>
2025-04-11 22:57:15 +0530

    Add test for newline delimiter

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,21 +1,25 @@
 require './string_calculator'
 
 RSpec.describe StringCalculator do
-    describe '#add' do
-        it 'returns 0 for an empty string' do
-            expect(StringCalculator.add("")).to eq(0)
-        end
+	describe '#add' do
+		it 'returns 0 for an empty string' do
+			expect(StringCalculator.add("")).to eq(0)
+		end
 
-        it 'returns the number itself for single number input' do
-            expect(StringCalculator.add("1")).to eq(1)
-        end
+		it 'returns the number itself for single number input' do
+			expect(StringCalculator.add("1")).to eq(1)
+		end
 
-        it 'returns the sum of two numbers separated by comma' do
-            expect(StringCalculator.add("1,2")).to eq(3)
-        end
+		it 'returns the sum of two numbers separated by comma' do
+			expect(StringCalculator.add("1,2")).to eq(3)
+		end
 
-        it 'returns the sum of multiple comma-separated numbers' do
-            expect(StringCalculator.add("1,2,3,4")).to eq(10)
-        end
-    end
+		it 'returns the sum of multiple comma-separated numbers' do
+			expect(StringCalculator.add("1,2,3,4")).to eq(10)
+		end
+
+		it 'handles newlines as delimiters' do
+			expect(StringCalculator.add("1\n2,3")).to eq(6)
+		end
+	end
 end
