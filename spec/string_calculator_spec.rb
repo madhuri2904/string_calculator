56d661c6f2ddaacae5646c9815bac39131ab2b57
madhuri2904 <banoriyamadhuri@gmail.com>
2025-04-11 22:13:38 +0530

    Add test for two comma-separated numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,5 +9,9 @@ RSpec.describe StringCalculator do
         it 'returns the number itself for single number input' do
             expect(StringCalculator.add("1")).to eq(1)
         end
+
+        it 'returns the sum of two numbers separated by comma' do
+            expect(StringCalculator.add("1,2")).to eq(3)
+        end
     end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -4,5 +4,7 @@ class StringCalculator
         return 0 if numbers.empty?
 
         return numbers.to_i if numbers.match?(/^\d+$/)
+
+        numbers.split(',').map(&:to_i).sum
     end
 end
